09fc8d26f720f4f20c4f0d5a0f23f998af6ee641
Vin_04 <32927048+VinayakBorhade@users.noreply.github.com>
2018-05-13 22:51:07 +0530

    Add files via upload

---
+++ dbms/SQLFiles/TABLES.sql
@@ -0,0 +1,33 @@
+Schema of all 5 Tables used-
+
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    dp_ext varchar(10)
+);
+CREATE TABLE following(
+ user_id INT,
+ follower_id INT);
+CREATE TABLE repositories_table(
+ id INT PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(100),
+ tags VARCHAR(100),
+ description VARCHAR(100),
+ username VARCHAR(50)
+);
+CREATE TABLE history_repositories_table(
+ id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ activity VARCHAR(100),
+ dt VARCHAR(100),
+ username VARCHAR(50)
+);
+CREATE TABLE upload_data(
+ id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ file_name VARCHAR(200),
+ file_size VARCHAR(200),
+ file_type VARCHAR(200),
+ user_id VARCHAR(50),
+ repo_name VARCHAR(50)
+);
